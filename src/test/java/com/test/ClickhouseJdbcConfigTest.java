bf746496d7f90c3f799233dc3a492b89f6a96173
xushanshan <1337220620@qq.com>
2020-06-10 16:16:03 +0800

    测试用例

--- src/test/java/com/test/ClickhouseJdbcConfigTest.java
+++ src/test/java/com/test/ClickhouseJdbcConfigTest.java
@@ -7,9 +7,6 @@ import java.sql.Statement;
 
 public class ClickhouseJdbcConfigTest {
     public static void main(String[] args) throws Exception {
-//        Properties properties = ClickhouseJdbcUrlParser.parse("jdbc:clickhouse://172.20.216.156:8123", new Properties()).asProperties();
-//        Properties properties = ClickhouseJdbcUrlParser.parse("jdbc:clickhouse://172.20.216.156:8123/Insight_datafarm_test_wei", new Properties()).asProperties();
-//        String database = properties.getProperty(ClickHouseQueryParam.DATABASE.getKey());
 
         Connection connection = DriverManager.getConnection("jdbc:clickhouse://localhost:8123");
 
